195528cb8062185143ffedbcfac4ad92c75cd734
Jordan Hudgens <jordan@devcamp.com>
2017-06-20 16:41:58 +0000

    Practice commit with master

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-      @posts = Blog.all
-  get 'pages/about'
-
-  get 'pages/contact'
-
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
